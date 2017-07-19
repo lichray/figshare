278ad4280347eff9e39902c148666531cb088e8d
Zhihao Yuan <lichray@gmail.com>
2017-07-19 00:07:18 -0500

    bump version and install dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 from distutils.core import setup
 
+def localopen(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
 setup(name='figshare',
-      version='0.1.2',
+      version='0.1.3',
       packages=['figshare'],
       author='Robert T. McGibbon',
       author_email='rmcgibbo@gmail.com',
       url='https://github.com/rmcgibbo/figshare',
       description='Command-line client for figshare API',
       license='MIT',
+      install_requires=localopen('requirements.txt').readlines(),
       scripts=['scripts/figshare'])
